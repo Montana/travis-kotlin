57c66fb510a9a5004f4ba8a2e33743885d8d1b8e
Michael Mendy <montana@linux.com>
2023-12-28 02:15:29 -0800

    Add `symlink_check.sh`

---
+++ symlink_check.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+grep -R -e '^\.\./\.\.' exercises | cut -d: -f1 - | while IFS= read -r file
+do
+    dir=$(dirname "$file")
+    filename=$(basename "$file")
+    target=$(cat "$file")
+
+    echo "Replacing ${file} with symlink to ${target}."
+
+    pushd "$dir" || continue
+    rm -f "$filename"
+    ln -s "$target" "$filename"
+    popd || continue
+
+    unset dir filename
+done
