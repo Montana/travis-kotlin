1039cfb5d651631773f85ced9901c6e63f76a4a7
Michael Mendy <montana@linux.com>
2024-01-02 11:43:47 -0800

    push `Coroutines`

---
+++ Coroutines.kt
@@ -0,0 +1,30 @@
+import kotlinx.coroutines.CompletableDeferred
+
+class BluetoothDiscotech(private val bluetoothInterface: BluetoothInterface) {
+
+    suspend fun discoverize() = beginDiscovery()
+        .run { collectDevices() }
+
+    private fun beginDiscovery() = if (bluetoothInterface.isEnabled) {
+        bluetoothInterface.startDiscovery()
+    } else {
+        bluetoothInterface.enable()
+        bluetoothInterface.bluetoothEnabled + ::onBluetoothEnabled
+    }
+
+    private suspend fun collectDevices() = mutableListOf<BloothDevice>()
+        .also { devices ->
+            bluetoothInterface.deviceDiscovered + { devices += it }
+            waitForDiscoveryToFinish()
+        }
+
+    private suspend fun waitForDiscoveryToFinish() = CompletableDeferred<Unit>()
+        .apply { bluetoothInterface.discoveryEnded + { complete(Unit) } }
+        .await()
+
+    private fun onBluetoothEnabled(unit: Unit) {
+        bluetoothInterface.startDiscovery()
+        bluetoothInterface.bluetoothEnabled - ::onBluetoothEnabled
+    }
+
+}
